197714b41729484721eee3fa38648f967e9b46e0
romanbaptista <79450213+romanbaptista@users.noreply.github.com>
2024-11-13 07:35:11 +0000

    Update 9_canada.py

--- pages/9_canada.py
+++ pages/9_canada.py
@@ -18,8 +18,8 @@ with mcol:
     st.write('<h1 style="font-family: Consolas, sans-serif; font-size: 60px; font-weight: normal;">CANADA Project</h1>', unsafe_allow_html=True)
     st.write('\n')
     st.write('\n')
-    st.write('\n')
-    st.write('\n')
+    # st.write('\n')
+    # st.write('\n')
 
     st.image('assets/images/ninth.png')
 
