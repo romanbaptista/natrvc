49a6cf80713a3f99ab784bae155992689e5d93ef
romanbaptista <79450213+romanbaptista@users.noreply.github.com>
2024-11-13 07:34:48 +0000

    Update 8_using_ai.py

--- pages/8_using_ai.py
+++ pages/8_using_ai.py
@@ -18,8 +18,8 @@ with mcol:
     st.write('<h1 style="font-family: Consolas, sans-serif; font-size: 60px; font-weight: normal;">Using AI</h1>', unsafe_allow_html=True)
     st.write('\n')
     st.write('\n')
-    st.write('\n')
-    st.write('\n')
+    # st.write('\n')
+    # st.write('\n')
 
     st.video('./assets/videos/anim5.mp4', autoplay = True, loop = True, start_time = 0)
 
